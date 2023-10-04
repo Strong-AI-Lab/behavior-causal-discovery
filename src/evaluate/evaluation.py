1a3866f153d50e97735ee504da02345aa9d4fe9a
ggendro <g.gendron35@gmail.com>
2023-10-04 02:26:59 +0000

    normalized mutual information to proper scale

--- src/evaluate/evaluation.py
+++ src/evaluate/evaluation.py
@@ -57,18 +57,19 @@ def mutual_information(model, loader, num_var, masked_idxs):
     # Compute probabilities p(y_pred) and p(y)
     y_preds = torch.cat(y_preds, dim=0)
     ys = torch.cat(ys, dim=0)
-
+    
     p_y_pred = torch.mean(y_preds, dim=0)
     p_y = torch.mean(ys, dim=0)
 
     # Calculate joint probability p(y_pred, y) = p(y_pred|y) * p(y). y_pred and y are not independent because they have a common cause x. we cannot buld a shared histogram because y_pred contains probabilities and y contains one-hot vectors.
-    y_uniques = torch.unique(ys, dim=0) # Select all unique values of y
+    remaining_vars = num_var - len(masked_idxs)
+    y_uniques = torch.unique(ys, dim=0) # Select all unique values of y (/!\ unstated assumption: all values of y are present in the dataset)
     y_freq = torch.stack([torch.sum(torch.all(ys == y_value, dim=-1)) for y_value in y_uniques], dim=0) / ys.size(0) # Compute the frequency of each unique value of y: p(y)
     y_pred_given_y = torch.stack([torch.stack([v[1] for v in torch.stack([ys,y_preds],-2) if (v[0] == y).all()]).mean(0) for y in y_uniques]) # Compute the probability of y_pred given each unique value of y: p(y_pred|y)
-    joint_p_y_y_pred = (y_freq.unsqueeze(1).repeat(1,y_pred_given_y.size(-1)) * y_pred_given_y).view(((num_var - len(masked_idxs))**2,))# Compute the joint probability of y_pred and y: p(y_pred, y) = p(y_pred|y) * p(y)
+    joint_p_y_y_pred = (y_freq.unsqueeze(1).repeat(1,y_pred_given_y.size(-1)) * y_pred_given_y).view((remaining_vars**2,))# Compute the joint probability of y_pred and y: p(y_pred, y) = p(y_pred|y) * p(y)
 
     # Calculate mutual information I(y_pred; y) = H(y_pred) + H(y) - H(y_pred, y)
-    mi = entropy(p_y_pred, num_var) + entropy(p_y, num_var) - entropy(joint_p_y_y_pred, num_var)
+    mi = entropy(p_y_pred, remaining_vars) + entropy(p_y, remaining_vars) - entropy(joint_p_y_y_pred, remaining_vars)
 
     return mi
 
